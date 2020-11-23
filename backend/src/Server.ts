0f2b218a55460f980163875dbfe6875c111e6ca6
koreaaerospaceU <zx811@naver.com>
2020-11-24 00:14:24 +0900

    typeorm mysql 환경설정

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -7,7 +7,7 @@ import express, { NextFunction, Request, Response } from "express"
 import StatusCodes from "http-status-codes"
 import "express-async-errors"
 
-import BaseRouter from "./routes"
+import BaseRouter from "./routes/api"
 import logger from "@shared/Logger"
 
 const app = express()
@@ -21,10 +21,34 @@ app.use(
         credentials: true,
     })
 )
+
 // body-parser 추가
 import bodyParser from "body-parser"
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// DB Connection Test
+import "reflect-metadata"
+import { createConnection } from "typeorm"
+import { User } from "./entity/User"
+
+createConnection()
+    .then(async (connection) => {
+        console.log("Inserting a new user into the database...")
+        const user = new User()
+        user.firstName = "Timber"
+        user.lastName = "Saw"
+        user.age = 25
+        await connection.manager.save(user)
+        console.log("Saved a new user with id: " + user.id)
+
+        console.log("Loading users from the database...")
+        const users = await connection.manager.find(User)
+        console.log("Loaded users: ", users)
+
+        console.log("Here you can setup and run express/koa/any other framework.")
+    })
+    .catch((error) => console.log(error))
 
 /************************************************************************************
  *                              Set basic express settings
